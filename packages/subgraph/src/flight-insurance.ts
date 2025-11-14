228b96284ed8c99c50888b9b862a5a10459b3e7c
TommyS725 <120657256+TommyS725@users.noreply.github.com>
2025-11-15 01:22:27 +0800

    Initialize subgraph

--- packages/subgraph/src/flight-insurance.ts
+++ packages/subgraph/src/flight-insurance.ts
@@ -1,19 +1,6 @@
+import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  FlightSettled as FlightSettledEvent,
-  InsurancePurchased as InsurancePurchasedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  PolicyCreated as PolicyCreatedEvent,
-  PolicyStatusChanged as PolicyStatusChangedEvent,
-  TokenListed as TokenListedEvent,
-  TokenProcessed as TokenProcessedEvent,
-  TokenPurchased as TokenPurchasedEvent,
-  TokenUnlisted as TokenUnlistedEvent,
-  Transfer as TransferEvent,
-  Withdrawal as WithdrawalEvent
-} from "../generated/FlightInsurance/FlightInsurance"
-import {
+  FlightInsurance,
   Approval,
   ApprovalForAll,
   FlightSettled,
@@ -27,201 +14,97 @@ import {
   TokenUnlisted,
   Transfer,
   Withdrawal
-} from "../generated/schema"
+} from "../generated/FlightInsurance/FlightInsurance"
+import { ExampleEntity } from "../generated/schema"
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
+export function handleApproval(event: Approval): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = ExampleEntity.load(event.transaction.from)
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new ExampleEntity(event.transaction.from)
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
   entity.owner = event.params.owner
   entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
+  // Entities can be written to the store with `.save()`
   entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.balanceOf(...)
+  // - contract.createPolicy(...)
+  // - contract.flightDelayContract(...)
+  // - contract.flightStates(...)
+  // - contract.getApproved(...)
+  // - contract.getFlightTokens(...)
+  // - contract.getPolicy(...)
+  // - contract.getTokenPolicy(...)
+  // - contract.isApprovedForAll(...)
+  // - contract.name(...)
+  // - contract.owner(...)
+  // - contract.ownerOf(...)
+  // - contract.policies(...)
+  // - contract.policyCounter(...)
+  // - contract.relayers(...)
+  // - contract.requestFlightDataByFlight(...)
+  // - contract.requiredReserve(...)
+  // - contract.subscriptionId(...)
+  // - contract.supportsInterface(...)
+  // - contract.symbol(...)
+  // - contract.tokenCounter(...)
+  // - contract.tokenListedPrice(...)
+  // - contract.tokenOwnerRequestFlightData(...)
+  // - contract.tokenPrices(...)
+  // - contract.tokenProcessed(...)
+  // - contract.tokenToPolicy(...)
+  // - contract.tokenURI(...)
 }
 
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
+export function handleApprovalForAll(event: ApprovalForAll): void {}
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+export function handleFlightSettled(event: FlightSettled): void {}
 
-  entity.save()
-}
+export function handleInsurancePurchased(event: InsurancePurchased): void {}
 
-export function handleFlightSettled(event: FlightSettledEvent): void {
-  let entity = new FlightSettled(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.flightIdHash = event.params.flightIdHash
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+export function handlePolicyCreated(event: PolicyCreated): void {}
 
-  entity.save()
-}
+export function handlePolicyStatusChanged(event: PolicyStatusChanged): void {}
 
-export function handleInsurancePurchased(event: InsurancePurchasedEvent): void {
-  let entity = new InsurancePurchased(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-  entity.policyId = event.params.policyId
-  entity.buyer = event.params.buyer
+export function handleTokenListed(event: TokenListed): void {}
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+export function handleTokenProcessed(event: TokenProcessed): void {}
 
-  entity.save()
-}
+export function handleTokenPurchased(event: TokenPurchased): void {}
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+export function handleTokenUnlisted(event: TokenUnlisted): void {}
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+export function handleTransfer(event: Transfer): void {}
 
-  entity.save()
-}
-
-export function handlePolicyCreated(event: PolicyCreatedEvent): void {
-  let entity = new PolicyCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.policyId = event.params.policyId
-  entity.flightNumber = event.params.flightNumber
-  entity.departureDate = event.params.departureDate
-  entity.flightIdHash = event.params.flightIdHash
-  entity.delayMinutesThreshold = event.params.delayMinutesThreshold
-  entity.expirationTimestamp = event.params.expirationTimestamp
-  entity.payoutAmount = event.params.payoutAmount
-  entity.inventory = event.params.inventory
-  entity.price = event.params.price
-  entity.open = event.params.open
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlePolicyStatusChanged(
-  event: PolicyStatusChangedEvent
-): void {
-  let entity = new PolicyStatusChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.policyId = event.params.policyId
-  entity.open = event.params.open
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTokenListed(event: TokenListedEvent): void {
-  let entity = new TokenListed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-  entity.price = event.params.price
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTokenProcessed(event: TokenProcessedEvent): void {
-  let entity = new TokenProcessed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-  entity.payoutAmount = event.params.payoutAmount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTokenPurchased(event: TokenPurchasedEvent): void {
-  let entity = new TokenPurchased(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-  entity.buyer = event.params.buyer
-  entity.price = event.params.price
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTokenUnlisted(event: TokenUnlistedEvent): void {
-  let entity = new TokenUnlisted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleWithdrawal(event: WithdrawalEvent): void {
-  let entity = new Withdrawal(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+export function handleWithdrawal(event: Withdrawal): void {}
